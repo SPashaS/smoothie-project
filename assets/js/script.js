56223eb505c3def022afc6abf7a4521329e1d9d7
SPashaS <SPavel.LD@gmail.com>
2021-12-17 23:52:34 +0300

    js menu start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+function burgerMenu() {
+    let burgerIcon = document.querySelector('.header__burger-btn');
+    let headerNavigation = document.querySelector('.header__navigation');
+    // let closeIcon = document.querySelector('')
+
+
+    burgerIcon.addEventListener('click', function() {
+        console.log('zdraste')
+        // headerNavigation.classList.toogle('.header__navigation-active')
+    })
+
+
+}
+
+
+burgerMenu();
 
 
 //# sourceMappingURL=script.js.map
