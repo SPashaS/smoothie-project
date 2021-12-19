27d3ad126a08f0a31154673003cf70014b1ca57d
SPashaS <SPavel.LD@gmail.com>
2021-12-19 20:58:28 +0300

    anchors go top

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -8,8 +8,31 @@ function burgerMenu() {
         burgerIcon.classList.toggle('header__burger-btn-close')
     })
 }
-
-
 burgerMenu();
+
+
+
+let anchors = document.querySelectorAll("a[href*='#']");
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        let blockID = anchor.getAttribute("href");
+        document.querySelector(blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
+
+const goTop = document.querySelector(".goTop");
+
+window.addEventListener("scroll", function () {
+    if (this.window.scrollY > 400) {
+        goTop.style.display = "block"
+    } else {
+        goTop.style.display = "none"
+    }
+})
 
 //# sourceMappingURL=script.js.map
