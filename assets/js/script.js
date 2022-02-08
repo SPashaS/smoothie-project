028b17c4badc5d3e0c7fd83e06c50bdfe68d07ba
SPashaS <SPavel.LD@gmail.com>
2022-02-08 19:46:38 +0300

    swap image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 function burgerMenu() {
     let burgerIcon = document.querySelector('.header__burger');
     let headerNavigation = document.querySelector('.header__navigation');
-
+    let body = document.querySelector('body');
     burgerIcon.addEventListener('click', function() {
-        headerNavigation.classList.toggle('header__navigation--active')
-        burgerIcon.classList.toggle('header__burger--active')
+        headerNavigation.classList.toggle('header__navigation--active');
+        burgerIcon.classList.toggle('header__burger--active');
+        body.classList.toggle('_lock');
     })
 }
 burgerMenu();
 
 
 
-let anchors = document.querySelectorAll("a[href*='#']");
-
+let anchors = document.querySelectorAll("a[href^='#']");
+console.log (anchors);
 for (let anchor of anchors) {
     anchor.addEventListener("click", function(e) {
         e.preventDefault();
